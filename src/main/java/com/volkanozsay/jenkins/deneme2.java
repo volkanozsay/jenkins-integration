d9a812bb2daea27d6d0615999c4d4c8b15a97a8a
Volkan OZSAY <volkanozsay@gmail.com>
2020-01-06 11:54:06 +0300

    create deneme2 class

---
+++ src/main/java/com/volkanozsay/jenkins/deneme2.java
@@ -0,0 +1,13 @@
+package com.volkanozsay.jenkins;
+
+public class deneme2 {
+ private String Deneme;
+
+    public String getDeneme() {
+        return Deneme;
+    }
+
+    public void setDeneme(String deneme) {
+        Deneme = deneme;
+    }
+}
