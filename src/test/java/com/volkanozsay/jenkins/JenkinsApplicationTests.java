e75f4f5d568567c72e248a144eadc302717e3cc4
Volkan OZSAY <volkanozsay@gmail.com>
2020-01-06 11:37:42 +0300

    jenkins integration

---
+++ src/test/java/com/volkanozsay/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkanozsay.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
